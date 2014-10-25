c2584e74b981098d144ab0fde4f9ed943cd26ca1
David Jegat <david.jegat@gmail.com>
2014-10-25 21:47:14 +0200

    Add a validation test case

--- funk/SimpleLine.php
+++ funk/SimpleLine.php
@@ -8,6 +8,10 @@ use Knp\Minibus\Minibus;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Knp\Minibus\Simple\Line;
 use Knp\Minibus\Station;
+use Knp\Minibus\Expectation\OpeningGateExpectation;
+use Knp\Minibus\Expectation\ClosingGateExpectation;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Knp\Minibus\Event\Subscriber\ExpectationStationSubscriber;
 
 class SimpleLine implements Spec
 {
@@ -29,21 +33,54 @@ class SimpleLine implements Spec
             'foo'    => true,
         ]);
     }
+
+    function it_can_validate_validable_station()
+    {
+        $minibus = new SimpleMinibus;
+        $dispatcher = new EventDispatcher;
+        $dispatcher->addSubscriber(new ExpectationStationSubscriber);
+
+        $busLine = new Line(new EventDispatcher);
+
+        $sampleStation = new SampleStation;
+        $fooStation    = new FooStation;
+
+        $busLine->addStation($sampleStation);
+        $busLine->addStation($fooStation);
+
+        $busLine->follow($minibus);
+
+        expect($minibus->getPassengers())->toReturn([
+            'sample' => true,
+            'foo'    => true,
+        ]);
+    }
 }
 
 
-class SampleStation implements Station
+class SampleStation implements Station, ClosingGateExpectation
 {
     public function handle(Minibus $minibus)
     {
         $minibus->addPassenger('sample', true);
     }
+
+    public function setLeavingPassengers(OptionsResolverInterface $resolver)
+    {
+        $resolver->setRequired(['sample']);
+        $resolver->setAllowedTypes(['sample' => 'bool']);
+    }
 }
 
-class FooStation implements Station
+class FooStation implements Station, OpeningGateExpectation
 {
     public function handle(Minibus $minibus)
     {
         $minibus->addPassenger('foo', true);
+    }
+
+    public function setEnteringPassengers(OptionsResolverInterface $resolver)
+    {
+        $minibus->setRequired(['sample']);
     }
 }
